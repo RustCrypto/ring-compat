1f4159badab70cb8e21a5addf01c37c491ac10cd
Tony Arcieri <bascule@gmail.com>
2020-10-09 06:16:51 -0700

    Make `aead` and `digest` features optional
    ...but enabled by default.
    
    This allows enabling select algorithm families as cargo features, which
    avoids pulling in the related RustCrypto dependencies when disabled.

--- tests/digest_tests.rs
+++ tests/digest_tests.rs
@@ -1,6 +1,38 @@
-use digest::generic_array::typenum::Unsigned;
+//! Digest tests
+
+#![cfg(feature = "digest")]
+
+use digest::{
+    dev::{digest_test, one_million_a},
+    generic_array::typenum::Unsigned,
+    new_test,
+};
 use digest::{BlockInput, FixedOutput};
 use ring_compat::digest::*;
+
+new_test!(sha1_main, "sha1", Sha1, digest_test);
+new_test!(sha256_main, "sha256", Sha256, digest_test);
+new_test!(sha384_main, "sha384", Sha384, digest_test);
+new_test!(sha512_main, "sha512", Sha512, digest_test);
+new_test!(sha512_256_main, "sha512_256", Sha512Trunc256, digest_test);
+
+#[test]
+fn sha1_1million_a() {
+    let output = include_bytes!("data/one_million_a.bin");
+    one_million_a::<Sha1>(output);
+}
+
+#[test]
+fn sha256_1million_a() {
+    let output = include_bytes!("data/sha256_one_million_a.bin");
+    one_million_a::<Sha256>(output);
+}
+
+#[test]
+fn sha512_1million_a() {
+    let output = include_bytes!("data/sha512_one_million_a.bin");
+    one_million_a::<Sha512>(output);
+}
 
 #[test]
 fn test_block_len() {
